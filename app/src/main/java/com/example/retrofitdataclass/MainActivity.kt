387926903e3663f3ef2b5f181fb3c077ff8af0dd
Weatherappppppp <ldvdasha@icloud.com>
2024-11-29 21:57:36 +0900

    Initial commit

---
+++ app/src/main/java/com/example/retrofitdataclass/MainActivity.kt
@@ -0,0 +1,55 @@
+package com.example.retrofitdataclass
+
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import android.view.Menu
+import android.view.MenuItem
+import androidx.core.view.WindowCompat
+import com.example.retrofitdataclass.databinding.ActivityMainBinding
+import com.example.retrofitdataclass.retrofit.MainApi
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val retrofit = Retrofit.Builder()
+            .baseUrl("https://api.weatherapi.com/v1/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+
+
+        binding.request.setOnClickListener {
+            CoroutineScope(Dispatchers.Main).launch {
+                val api = retrofit.create(MainApi::class.java)
+                val model = api.getWeatherData(
+                    "d6c16bc95c564babb9e35938242109",
+                    "Yakutsk",
+                    "3",
+                    "no",
+                    "no"
+
+                )
+                binding.temp.text = model.current.temp_c.toString()
+                binding.date.text = model.location.localtime
+            }
+
+        }
+
+    }
+}
